9850788de911f315caeccda68cda35cc361a49ed
dpindrys <david@viridislearning.com>
2014-04-23 19:03:17 -1000

    big push. home page and styling css

--- wp-content/themes/foundation/page-home.php
+++ wp-content/themes/foundation/page-home.php
@@ -21,48 +21,48 @@ get_header(); ?>
 
 			<div class="home-hero">
 				<div class="row interior">
-					<div class="medium-12 columns">
-						<h1 class="hero-type text-center">Trusted solutions, dedicated delivery in:</h2>
-						<hr>
-						<h2 class="hero-type text-center">Agriculture • Golf • Irrigation • Waterworks</h2?
+					<div class="medium-6 columns">
+						<h1>Trusted solutions, dedicated delivery.</h1>
+					</div>
+					<div class="medium-6 columns">
+						<ul class="industries">
+							<li>Agriculture</li> 
+							<li>Golf</li>
+							<li>Irrigation</li>
+							<li>Waterworks</li>
+						</ul>
 					</div>
 					<div class="medium-6 columns text-left">
 
 					</div>
 				</div>
 			</div>
-			</div>
+		
 			<!--3 buttons-->
-			<div class="row interior brand">
-			<div class="medium-4 columns">
+			<div class="row interior white">
+			<div class="small-4 columns">
 			<a href="?page_id=25">
 			<div class="front-field">
 			<img class="about-button" src="wp-content/themes/foundation/includes/assets/about_button.png">
-			<h4 class="text-center">About Us</h4>
-			<hr class="small-thick">
-			<p>Meet our team members and find out what sets us apart from other distributors.</p>
+			<h4 class="text-center hide-for-small-only">About Us</h4>
 			</div>
 			</a>
 			</div>
 
-						<div class="medium-4 columns">
+						<div class="small-4 columns">
 			<a href="?page_id=14">
 			<div class="front-field">
 			<img class="about-button" src="wp-content/themes/foundation/includes/assets/vendors_button.png">
-			<h4 class="text-center">Our Vendors</h4>
-			<hr class="small-thick">
-			<p>Browse acategorized list of all the products that are supplied by our world class manufacturers.</p>
+			<h4 class="text-center hide-for-small-only">Our Vendors</h4>
 			</div>
 			</a>
 			</div>
 
-			<div class="medium-4 columns">
+			<div class="small-4 columns">
 			<a href="?post_type=tribe_events">
 			<div class="front-field">
 			<img class="about-button" src="wp-content/themes/foundation/includes/assets/events_button.png">
-			<h4 class="text-center">Training & Events</h4>
-			<hr class="small-thick">
-			<p>Take a look at some of our past projects we’ve helped make possible and imagine what we could do for you.</p>
+			<h4 class="text-center hide-for-small-only">Training & Events</h4>
 			</div>
 			</a>
 			</div>
