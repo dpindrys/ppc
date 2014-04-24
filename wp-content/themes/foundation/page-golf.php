e44bd1c71957b1786c41cc93f79d1fefc89658b1
Ashwin Ramesh <ash.mirage@gmail.com>
2014-04-24 14:29:21 +0530

    Display product name grouped by category with proper markup

--- wp-content/themes/foundation/page-golf.php
+++ wp-content/themes/foundation/page-golf.php
@@ -4,87 +4,11 @@ Template Name: Golf Page
 */
 get_header(); ?>
 <?php
- //  $postsa = get_posts(array('post_type'=>'products'));
-	// foreach($postsa as $p) {
-	// 				echo "\n";
-	//         $pc = get_post_complete($p->ID);
-	//         // get_post_complete() returns data as an ARRAY, not as an OBJECT!!!
-	//         print $pc['post_title'];  
-	//         print $pc['my_custom_field'];
-	//         print_r($pc); // <-- prints out all available fields, both built-in and custom fields
-	//         echo "_______________________________________________________________________________________";
-	//         echo "_______________________________________________________________________________________";
-	//         echo "_______________________________________________________________________________________";
-	// }
-
-	// if ( is_category( 'golf-slug' ) ) : 
-	// 	echo "asdfas";
-	// 	$postsa = get_posts( array( 'category_name' => 'club', 'posts_per_page' => -1 ) ); 
-
-	// endif;
-	// foreach($postsa as $p) {
-	// 				echo "\n";
-	//         $pc = get_post_complete($p->ID);
-	//         // get_post_complete() returns data as an ARRAY, not as an OBJECT!!!
-	//         print $pc['post_title'];  
-	//         print $pc['my_custom_field'];
-	//         print_r($pc); // <-- prints out all available fields, both built-in and custom fields
-	//         echo "_______________________________________________________________________________________";
-	//         echo "_______________________________________________________________________________________";
-	//         echo "_______________________________________________________________________________________";
-	// }
-	
-
-
-//	Get Parent Category Type
-	$catID = get_cat_id('golf');
-	//	Filter by Type
-	// $args = array( 'post_type' => 'products', 'posts_per_page' => 10, 'cat' => $catID );
-	// $postsa = get_posts( $args );
-
-//	Get Sub categories for parent
-	$sub_categories = get_categories('&title_li=&show_count=1&child_of='.$catID);
-
-//	For each subcategory,
-	foreach($sub_categories as $sub_c) {
-		//	- Get product entries
-		$args = array( 'post_type' => 'products', 'cat' => $sub_c->cat_ID );
-		$products_in_sub_category = get_posts( $args );
-		echo "<h4>" . $sub_c->name . "</h4>";
-
-		//	- Vendor information
-		foreach($products_in_sub_category as $p) {
-    	$pc = get_post_complete($p->ID);
-    	// print_r($pc);
-
-    	// $args = array( 'post_type' => 'vendor', 'posts_per_page' => 10, 'ID' => $p->vendor );
-    	// $vendor = get_posts( $args );
-    	// print_r($vendor);
-
-    	// get_post_complete() // returns data as an ARRAY, not as an OBJECT!!!
-?>
-	  	<a href=" <?php echo $pc['URL']; ?> " target="_blank"> <?php echo $pc['post_title'] ?> </a> <br>
-
-<?php
-			// $posta = new WP_Query( $args );
-      // print $pc['my_custom_field'];
-      // print_r($pc); // <-- prints out all available fields, both built-in and custom fields
-		}	//	End of inner loop
-  }	//	End of outer loop
+//  Get Parent Category Type
+  $catID = get_cat_id('golf');
 ?>
 
-
-  <!-- while ( $posta->have_posts() ) : $posta->the_post(); -->
-  	<!-- the_title(); -->
-  	<!-- echo '<div class="entry-content">'; -->
-  	<!-- the_content(); -->
-  	<!-- echo '</div>'; -->
-  <!-- endwhile; -->
-
-
-
-
-<div class="row interior white toppadding1">
+	<div class="row interior white toppadding1">
       <div class="medium-9 columns"><h1>Golf</h1><hr>
       <p>HD Supply Waterworks is the nation’s largest distributor of water, sewer, storm and fire protection products. We deliver the quality products and superior services our customers demand to get the job done. Our highly knowledgeable associates understand the specific needs of our customers and can be relied upon to provide personal touch service.</p></div>
       <div class="medium-3 columns">
@@ -98,28 +22,82 @@ get_header(); ?>
           <li class=""><a href="?page_id=8">Waterworks</a></li>
 
         </ul>
-</div>
-
-	
-	<?php /* Start loop */ ?>
-	<?php while (have_posts()) : the_post(); ?>
-		<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
-			<!--
-			<header>
-				<h1 class="entry-title"><?php the_title(); ?></h1>
-			</header>
-			-->
-			<div class="entry-content">
-				<?php the_content(); ?>
 			</div>
-			<footer>
-				<?php wp_link_pages(array('before' => '<nav id="page-nav"><p>' . __('Pages:', 'FoundationPress'), 'after' => '</p></nav>' )); ?>
-				<p><?php the_tags(); ?></p>
-			</footer>
-			<?php comments_template(); ?>
-		</article>
-	<?php endwhile; // End the loop ?>
-
 	</div>
-		
+	
+	<div class="row interior white">
+	  <div class=" medium-12 columns">
+	    <div class="row toppadding1">
+	      <div class="medium-12 columns">
+	        
+	        <?php
+	          //  Filter by Type
+	          // $args = array( 'post_type' => 'products', 'posts_per_page' => 10, 'cat' => $catID );
+	          // $postsa = get_posts( $args );
+
+	        //  Get Sub categories for parent
+	          $sub_categories = get_categories('&title_li=&show_count=1&child_of='.$catID);
+					if($catID != 0) {
+	        //  For each subcategory,
+	        foreach($sub_categories as $sub_c) {
+	          //  - Get product entries
+	          $args = array( 'post_type' => 'products', 'cat' => $sub_c->cat_ID );
+	          $products_in_sub_category = get_posts( $args );
+	        ?>
+	          <table>
+	           <thead>
+	              <tr>
+	                <th> <?php echo $sub_c->name; ?> </th>
+	                <th class="vendor-cell">Vendor</th>
+	              </tr>
+	            </thead>
+	            <tbody>
+	            <?php
+	              //  - Vendor information
+	              foreach($products_in_sub_category as $p) {
+	                $pc = get_post_complete($p->ID);
+	            ?>
+	              <tr>
+	                <td>
+	                  <a href=" <?php echo $pc['URL']; ?> " target="_blank"> <?php echo $pc['post_title'] ?> </a> <br>
+	                </td>
+	                <td class="product-logo"><a href="#"><img class="product-logo" src="img/products/ARI/logo.png"></a></td>
+	              </tr>
+	            <?php
+	              } //  End of inner loop
+	            ?>
+	            </tbody>
+	          </table>
+	        <?php
+	        } //  End of outer loop
+	      	} else {
+	      	?>
+	      		<table>
+	      		 <thead>
+	      		    <tr>
+	      		      <th> Category </th>
+	      		      <th class="vendor-cell">Vendor</th>
+	      		    </tr>
+	      		  </thead>
+	      		  <tbody>
+	      		  	<tr>
+	      		  	  <td>
+	      		  	    No Entries Found
+	      		  	  </td>
+	      		  	  <td>
+	      		  	    -
+	      		  	  </td>
+	      		  	</tr>
+	      		  </tbody>
+	      		</table>
+	      	<?php	
+	      	}// End of IF
+	        ?>
+
+	      </div>
+	    </div>
+	  </div>
+	</div>
+
+
 <?php get_footer(); ?>
